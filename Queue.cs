f2a80b3e429376895e97b622e2ad9146382c2eaa
TrentonSwe <89215748+TrentonSwe@users.noreply.github.com>
2022-10-25 20:00:39 -0500

    commit

---
+++ Queue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace PracticeExercise3
+{
+	public class Queue<T> : IQueue<T>
+	{
+        private LinkedList<T> linkedList;
+
+		public Queue()
+		{
+            linkedList = new LinkedList<T>();
+		}
+
+        public T First => IsEmpty ? throw new EmptyQueueException() : linkedList.First.Value;
+
+        public T Last => IsEmpty ? throw new EmptyQueueException() : linkedList.Last.Value;
+
+        public int Length => linkedList.Count;
+
+        public bool IsEmpty => Length == 0;
+
+        public T Dequeue()
+        {
+            if( IsEmpty )
+            {
+                throw new EmptyQueueException();
+            }
+
+            var front = linkedList.First.Value;
+            linkedList.RemoveFirst();
+
+            return front;
+        }
+
+        public void Enqueue(T item)
+        {
+            linkedList.AddLast(item);
+        }
+
+        public override string ToString()
+        {
+            string result = "<Back> ";
+
+            var currentNode = linkedList.Last;
+            while (currentNode != null)
+            {
+                result += currentNode.Value;
+                if (currentNode.Previous != null)
+                {
+                    result += " → ";
+                }
+                currentNode = currentNode.Previous;
+            }
+
+            result += " <Front>";
+
+            return result;
+        }
+    }
+}
+
